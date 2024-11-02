05494dd928b1aac0b4524af33119ba948469da85
Mykyta <korsikov_php@upiple.com>
2024-11-02 13:30:57 +0200

    [APP] add and list added

--- swift/Sources/MySwiftApp/Controller/TodoController.swift
+++ swift/Sources/MySwiftApp/Controller/TodoController.swift
@@ -5,9 +5,6 @@ class TodoController {
 
     @MainActor
     func handle(input: String) {
-
-        // TODO: if you remove this it will return error
-        // think out it more deeply...
         var arrayOfTodoItemObjects: [TodoItem]
 
         do {
@@ -17,16 +14,54 @@ class TodoController {
             return
         }
 
-
-        // TODO: complete CRED
-        // TODO: analyze syntax...
        switch input {
         case "add":
-            print("Not yet!")
-            // ... implement add method
+                print("Enter the title for your new todo:")
+                if let title = readLine(), title.isEmpty == false {
+                    var newTodoItem = TodoItem()
+                    newTodoItem.setTitle(title: title)
+                    arrayOfTodoItemObjects.append(newTodoItem)
+
+                    do {
+                        try fileManager.saveTasksToJsonFile(
+                            arrayOfTodoItemObjects: arrayOfTodoItemObjects
+                        )
+                    } catch {
+                        print("Error saving todos:", error)
+                        return
+                    }
+
+                    print("Saved!")
+                } else {
+                    print("No input!")
+                }
         case "list":
-            print("Not yet!")
-            // ... implement list method
+            var arrayOfTodoItemObjects: [TodoItem]
+
+            do {
+                arrayOfTodoItemObjects = try fileManager.loadTasksToObjects()
+            } catch {
+                print("Error loading todos:", error)
+                return
+            }
+
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+
+            var result: Data
+
+            do {
+                result = try encoder.encode(arrayOfTodoItemObjects)
+            } catch {
+                print("Error encoding loaded todos:", error)
+                return
+            }
+
+            if let result = String(data: result, encoding: .utf8) {
+                print("Your Todos:\n\(result)")
+            } else {
+                print("Failed to convert JSON data to string.")
+            }
         case "complete":
             print("Not yet!")
             // ... implement complete method
@@ -37,10 +72,6 @@ class TodoController {
             print("Unknown command")
         }
 
-
-
-        // save everything after changes
-        // or something....
         do {
             try fileManager.saveTasksToJsonFile(
                 arrayOfTodoItemObjects: arrayOfTodoItemObjects
